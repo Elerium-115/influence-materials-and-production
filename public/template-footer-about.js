131353dff0b69474a265237f1b751f7cec2fd4b1
Elerium-115 <elerium115+github@gmail.com>
2023-11-14 00:36:39 +0200

    Remove adalia.id logos

--- public/template-footer-about.js
+++ public/template-footer-about.js
@@ -5,23 +5,17 @@
  */
 export const templateFooterAbout = /*html*/ `
     <div class="footer-about">
-    <div class="title">
-        Materials & Production Tools
-    </div>
-    <div class="primary">
-        created by <a href="https://twitter.com/elerium115" target="_blank" title="Elerium115">Elerium115</a>
-    </div>
-    <div class="secondary extra-info">
-        for <a href="https://www.influenceth.io/" target="_blank" title="Influence">
-            Influence<br>
-            <span class="influence-logo-icon"></span>
-        </a>
-    </div>
-    <div class="secondary" style="display: none;">
-        hosted by <a href="https://my.adalia.id" target="_blank" title="my.adalia.id">
-            my.adalia.id<br>
-            <img src="./img/adalia-id-logo.png" alt="my.adalia.id" class="footer-logo">
-        </a>
-    </div>
+        <div class="title">
+            Materials & Production Tools
+        </div>
+        <div class="primary">
+            created by <a href="https://twitter.com/elerium115" target="_blank" title="Elerium115">Elerium115</a>
+        </div>
+        <div class="secondary extra-info">
+            for <a href="https://www.influenceth.io/" target="_blank" title="Influence">
+                Influence<br>
+                <span class="influence-logo-icon"></span>
+            </a>
+        </div>
     </div>
 `;
