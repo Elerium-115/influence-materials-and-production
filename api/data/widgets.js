63cab5b1c00fbb648e50ad984f8420f4c81611cf
Elerium-115 <elerium115+github@gmail.com>
2024-06-30 13:09:51 +0300

    Widgets: Re-enable Marketplace Prices

--- api/data/widgets.js
+++ api/data/widgets.js
@@ -1,6 +1,6 @@
 const widgets = [
     {title: 'Shopping List', url: 'https://influence.elerium.dev/widgets/shopping-list.html', default: true},
-    // {title: 'Marketplace Prices', url: 'https://widgets.starkgames.io/prices/129'},
+    {title: 'Marketplace Prices', url: 'https://widgets.starkgames.io/prices/129'},
     {title: 'More coming soon...', url: null},
 ];
 
