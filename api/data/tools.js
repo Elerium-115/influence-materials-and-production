9736ba7b025923f54d937f9a5c58c6950ccc2565
Elerium-115 <elerium115+github@gmail.com>
2024-07-03 02:21:19 +0300

    Update community tools

--- api/data/tools.js
+++ api/data/tools.js
@@ -26,29 +26,32 @@ const tools = [
         items: [
             {title: 'Asteroids Planner', author: 'Elerium115', url: 'https://influence.elerium.dev/asteroids-planner.html'},
             {title: 'Asteroid Management', author: 'Tyrell-Yutani', url: 'https://tyrell-yutani.app/#/asteroid'},
+            {title: 'Asteroid Tracker & Visualizer', author: 'Daharius', url: 'https://influence.daharius.com/AsteroidsTracker'},
             {title: 'Asteroid Metadata Lookup by Daharius', author: 'Daharius', url: 'https://influence.daharius.com/AsteroidMetadata/1'},
             {title: 'Asteroid Metadata Lookup by trevis', author: 'trevis & Dirac', url: 'https://starksight.plus/collection/INFA/1'},
-            {title: 'Asteroid Tracker & Visualizer', author: 'Daharius', url: 'https://influence.daharius.com/AsteroidsTracker'},
-            {title: 'Adalia Co-orbital Rocks', author: 'RGR', url: 'https://adalia.coorbital.rocks/'},
+            // {title: 'Adalia Co-orbital Rocks', author: 'RGR', url: 'https://adalia.coorbital.rocks/'}, // offline
         ],
     },
     {
         category_short: 'Crew',
         category: 'Crew Tools',
         items: [
+            {title: 'Crew Tracker', author: 'Denker', url: 'https://www.adalia.info/tools/crew-tracker'},
             {title: 'Crew Member Services', author: 'Tyrell-Yutani', url: 'https://tyrell-yutani.app/#/crew'},
             {title: 'Crewmate Metadata Lookup by Daharius', author: 'Daharius', url: 'https://influence.daharius.com/CrewmateMetadata/1'},
             {title: 'Crewmate Metadata Lookup by trevis', author: 'trevis & Dirac', url: 'https://starksight.plus/collection/INFC/1'},
             // {title: 'Crew Planner', author: 'Daharius', url: 'https://influence.daharius.com/CrewPlanner'}, // removed as requested by Daharius @ 2024-03-19
             {title: 'Crew Builder', author: 'Grey Area', url: 'https://adalia.academy/POC/index.html'},
-            {title: 'Crew Tracker', author: 'Denker', url: 'https://www.adalia.info/tools/crew-tracker'},
-            {title: 'Action Planner', author: 'Elerium115', url: 'https://elerium-115.github.io/influence-css/tool-action-planner/'},
+            {title: 'Floodgate (hire crews)', author: 'Cheveuxxx & Denker', url: 'https://www.floodgate.space/'},
+            {title: 'Crewmate Trait & Affinity Bonuses', author: '[official]', url: 'https://wiki.influenceth.io/en/gameplay/crewmates/bonuses'},
+            {title: 'Action Planner (prototype)', author: 'Elerium115', url: 'https://elerium-115.github.io/influence-css/tool-action-planner/'},
         ],
     },
     {
         category_short: 'Flight',
         category: 'Flight Tools',
         items: [
+            {title: 'System Travels Summary', author: 'trevis & Dirac', url: 'https://starksight.plus/flights'},
             {title: 'Flight Chart Zoom (Chrome)', author: 'RGR', url: 'https://chromewebstore.google.com/detail/influence-flight-chart-zo/makjdbggcjkjclglokiopecpghgmkgba'},
             {title: 'Flight Chart Zoom (Firefox)', author: 'RGR', url: 'https://addons.mozilla.org/en-US/firefox/addon/influence-flight-zoom-chart/'},
         ],
@@ -57,12 +60,16 @@ const tools = [
         category_short: 'Information',
         category: 'Information Tools',
         items: [
-            {title: 'Wendash', author: 'Joosh & Admiral', url: 'https://influence.wendash.com/'},
+            {title: 'Influence Wiki', author: '[official]', url: 'https://wiki.influenceth.io/'},
+            // {title: 'Wendash', author: 'Joosh & Admiral', url: 'https://influence.wendash.com/'}, // password gated
             {title: 'Trader Dashboard', author: 'Denker', url: 'https://www.adalia.info/tools/trader-dashboard'},
+            {title: 'Marketplace Trades Summary', author: 'trevis & Dirac', url: 'https://starksight.plus/trades'},
+            {title: 'Resource Prices', author: 'trevis & Dirac', url: 'https://starksight.plus/trades/129/price'},
             {title: 'Adalia Academy', author: 'Grey Area & Skippy', url: 'https://adalia.academy/'},
-            {title: 'Influence Sales', author: 'Teandy', url: 'https://influence-sales.space/'},
+            // {title: 'Influence Sales', author: 'Teandy', url: 'https://influence-sales.space/'}, // offline
             {title: 'Adalia.Guide', author: 'Korivak', url: 'https://adalia.guide/'},
             {title: 'Adalia Info', author: 'Denker', url: 'https://adalia.info/'},
+            {title: 'StarkSight Topics', author: '[community]', url: 'https://starksight.plus/topics'},
             {title: 'Analytics on Dune', author: 'brakmaar', url: 'https://dune.com/brakmaar/Influence-asteroids'}, // blocked by "dune.com"
             // {title: 'Adalia.id', author: 'strwrsfrk & Myrhea', url: 'https://my.adalia.id/'}, // abandoned?
         ],
