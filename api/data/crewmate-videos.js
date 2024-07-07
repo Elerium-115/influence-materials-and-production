0532348eef668bf240a58bff8233d2585a77882e
Elerium-115 <elerium115+github@gmail.com>
2024-07-07 18:35:08 +0300

    Add video for crewmates 3547, 5097

--- api/data/crewmate-videos.js
+++ api/data/crewmate-videos.js
@@ -3,6 +3,8 @@ const crewmateVideosBaseUrl = 'https://influence.elerium.dev/assets/crewmate-vid
 const crewmateVideos = {};
 
 const crewmateIdsWithVideo = [
+    3547, // [1ST] Skippy The Magnificent (crew 2438)
+    5097, // [1ST] Skippy The Magnificent (crew 169)
     5807, // [1ST] Skippy The Magnificent (crew 159)
     20193, // Elerium 115 - Hans Yolo (crew 129)
 ];
