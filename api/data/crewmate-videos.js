91c3b37688a30016de47b08090f478b77ef1575c
Elerium-115 <elerium115+github@gmail.com>
2024-07-12 00:28:14 +0300

    Add video for crewmate 2426

--- api/data/crewmate-videos.js
+++ api/data/crewmate-videos.js
@@ -3,13 +3,14 @@ const crewmateVideosBaseUrl = 'https://influence.elerium.dev/assets/crewmate-vid
 const crewmateVideos = {};
 
 const crewmateIdsWithVideo = [
-    2428, // Elerium 115 (crew 3814)
+    2426, // Elerium 115 (crew 3814)
+    2428, // Elerium 115 (crew 4951)
     3547, // [1ST] Skippy The Magnificent (crew 2438)
     5097, // [1ST] Skippy The Magnificent (crew 169)
     5807, // [1ST] Skippy The Magnificent (crew 159)
     20193, // Elerium 115 (crew 129)
     27392, // Elerium 115 (crew 3574)
-    29933, // Elerium 115 (crew 4951)
+    29933, // Elerium 115 (crew 5515)
 ];
 
 crewmateIdsWithVideo.forEach(crewmateId => {
